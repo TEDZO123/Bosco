723a6c80165588331c2d0f4662591139db9d7233
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-02-26 07:39:43 +0530

    Update Bosco.js

--- Bosco.js
+++ Bosco.js
@@ -327,7 +327,7 @@ module.exports = bosco = async (bosco, mek) => {
                             "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=",
                             "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69",
                             "mediaKeyTimestamp": "1610993486",
-                            "jpegThumbnail": fs.readFileSync('./tedzo.jpg'),
+                            "jpegThumbnail": fs.readFileSync('./ds.jpg'),
                             "scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw=="
                         }
                     }
